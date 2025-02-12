0dcc72ab70eb22fa81ea200e45123e05544ed61c
Mayank Gupta <mayankg88813@gmail.com>
2025-02-12 13:42:50 +0530

    Login and Logout controller done

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -48,10 +48,41 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("Signup Route");
+export const login = async (req, res) => {
+    
+ try {
+  const {email,password} = req.body;
+  const user = await User.findOne({email});
+  if(!user) return res.status(400).json({message:"Invalid user credentials"})
+
+    const isPasswordCorrect = bcrypt.compare(password, user.password);
+    if(!isPasswordCorrect) return res.status(400).json({
+      message:"Invalid Password"
+    })
+ 
+    generateToken(user._id,res);
+
+    res.status(200).json(
+      {
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        profilePic: user.profilePic
+      }
+    )
+
+ } catch (error) {
+  console.log("Error in Login controller",error.message);
+  res.status(500).json({message:"Internal Server Error"});
+ }
 };
 
 export const logout = (req, res) => {
-  res.send("Signup Route");
+  try {
+    res.cookie("jwt","",{maxAge:0})
+    res.status(200).json({message:"Logout Successfully"});
+  } catch (error) {
+    console.log("Error in Logout controller",error.message);
+    res.status(500).json({message:"Internal Server Error"});
+  }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 import express from "express"
 import dotenv from "dotenv"
+ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js" 
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
+
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json())//this middleware allow us to extract data from req in json format.
 
 const PORT = process.env.PORT;
