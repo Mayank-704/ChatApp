9882540d85bed943f9291dc4072392a5b596c00b
Mayank Gupta <mayankg88813@gmail.com>
2025-02-16 12:21:33 +0530

    Done updateProfile controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import { generateToken } from "../lib/utils.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -86,3 +87,31 @@ export const logout = (req, res) => {
     res.status(500).json({message:"Internal Server Error"});
   }
 };
+
+export const updateProfile = async (req,res) =>{
+  try {
+    const {profilePic} = req.body;
+    const userId = req.user._id;
+
+    if(!profilePic) {
+      return res.status(400).json({message:"Profile Pic is required"});
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updateUser = User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url   //secureURL is a field provide by cloudinary
+      },
+      {
+        new: true
+      }
+    );
+
+    res.status(200).json(updateUser);
+
+  } catch (error) {
+       console.log("Error in update profile controller",error );
+       res.status(500).json({message:"Internal server error"});   
+  }
+}
